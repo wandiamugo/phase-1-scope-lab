5bd1e4510ee137aff6dc4c38bcb58177bf37f72c
Wandia E Mugo <wwandia173@gmail.com>
2022-06-09 16:02:23 +0300

    commit tree ccfd3aae95707dbf0071527e0d7425d879be300f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+  console.log(customerName);
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer= "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer= "maybe bob";
+}
+
+const leastFavoriteCustomer= "Assignment to constant variable.";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer= "ann";
+    return leastFavoriteCustomer;
+}
